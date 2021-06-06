0c5a0edb842255a593a680a668ab9da6d82b512c
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-06-06 14:52:43 -0500

    Loading data set

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,8 @@ console.log("app.js connected");
 // -------------------------------------------- //
 
 // Code below
+// d3.csv("../data").then(function(data) {
+//     console.log("Loading Data CSV");
+//     console.log(data);
+
+// });
