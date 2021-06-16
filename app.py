9e21f0b7b73363238a4214ee853195d98eff0dfd
Yonis Hassan <hyonis@fb.com>
2021-06-16 17:05:14 -0500

    update the app.py

--- app.py
+++ app.py
@@ -38,13 +38,39 @@ def projectOverview():
 # route that takes user input and makes a predictions
 @app.route("/predictor",methods=['Get','POST']) #,methods=['POST']
 def predictor():
+    #income prediction labels
+    prediction_labels = [">=50","<=50"]
 
-    colour = ['Red', 'Blue', 'Black', 'Orange']
+    # Load the model.
+    randomforest = load(open('randomforest.pkl', 'rb'))
+
+    # Load the scaler.
+    scaler = load(open('scaler.pkl', 'rb'))
+
+    #these values are from random_forest_ml.ipynb > input_14260,30339,8610 
+    prediction1 = [[27,160178,10,0,0,38,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]]
+
+    prediction2 = [[50,175804,13,0,0,40,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]]
+
+    prediction3 = [[24,322931,4,0,1902,40,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]]
+
+    #scaled values
+    prediction1_scaled = scaler.transform(prediction1)
+    prediction2_scaled = scaler.transform(prediction2)
+    prediction3_scaled = scaler.transform(prediction3)
+    
+    # need to connect to my buttons for this to work???
+    predict = randomforest.predict(prediction1_scaled)
+    print(f'Your income predction is {prediction_labels[predict[0]]}')
+
+    predict = randomforest.predict(prediction2_scaled)
+    print(f'Your income predction is {prediction_labels[predict[0]]}')
+
+    predict = randomforest.predict(prediction3_scaled)
+    print(f'Your income predction is {prediction_labels[predict[0]]}')
 
     
-    
-    
-    webpage = render_template("predictions_model.html" ,colour=colour)
+    webpage = render_template("predictions_model.html", )
     return webpage
 
 #this rought has the about team info
