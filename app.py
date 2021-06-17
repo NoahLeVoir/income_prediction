bc97bb7f90fd052829f49bf44282d91aaec3cadd
Yonis Hassan <hyonis@fb.com>
2021-06-16 21:42:23 -0500

    button works

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import joblib
 from pickle import load
+import numpy as np
+import random
+import csv
 
 
 # Placeholder code incase we use a db
@@ -36,9 +39,18 @@ def projectOverview():
 # We might have to move away from making any predcitons 
 # might delete this route
 # route that takes user input and makes a predictions
-@app.route("/predictor",methods=['Get','POST']) #,methods=['POST']
+@app.route("/predictor")
 def predictor():
-    #income prediction labels
+  
+    
+    webpage = render_template("predictions_model.html",)
+    return webpage
+
+
+#this rought has the about team info
+@app.route("/magic")
+def magicHappenshere():
+      #income prediction labels
     prediction_labels = [">=50","<=50"]
 
     # Load the model.
@@ -47,29 +59,22 @@ def predictor():
     # Load the scaler.
     scaler = load(open('scaler.pkl', 'rb'))
 
-    #these values are from random_forest_ml.ipynb > input_14260,30339,8610 
-    prediction1 = [[27,160178,10,0,0,38,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]]
-    prediction2 = [[50,175804,13,0,0,40,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]]
-    prediction3 = [[24,322931,4,0,1902,40,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]]
+    # add file here
+    with open('dataset.csv') as f:
+        reader = csv.reader(f)
+        chosen_row = random.choice(list(reader))
+        chosen_row_float = [float(x) for x in chosen_row ]
+        # print(chosen_row)
+        chosen_row_reshape = [np.array(chosen_row_float)]
+        # print(chosen_row_reshape)
+        
+        prediction_scaled = scaler.transform(chosen_row_reshape)
+        print(prediction_scaled)
 
-    #scaled values
-    prediction1_scaled = scaler.transform(prediction1)
-    prediction2_scaled = scaler.transform(prediction2)
-    prediction3_scaled = scaler.transform(prediction3)
-    
-    # # need to connect to my buttons for this to work???
-    # predict1 = randomforest.predict(prediction1_scaled)
-    # print(f'Your income predction is {prediction_labels[predict[0]]}')
+        # 
+        predict = randomforest.predict(prediction_scaled)
+        return prediction_labels[predict[0]]
 
-    # predict2 = randomforest.predict(prediction2_scaled)
-    # print(f'Your income predction is {prediction_labels[predict[0]]}')
-
-    # predict3 = randomforest.predict(prediction3_scaled)
-    # print(f'Your income predction is {prediction_labels[predict[0]]}')
-
-    
-    webpage = render_template("predictions_model.html",)
-    return webpage
 
 #this rought has the about team info
 @app.route("/team")
