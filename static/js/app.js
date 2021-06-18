af597898e8a5cdd2e2c1fffa7c73d495d8ebf0de
Yonis Hassan <hyonis@fb.com>
2021-06-18 10:31:25 -0500

    add index row to the predict output

--- static/js/app.js
+++ static/js/app.js
@@ -4,12 +4,10 @@ console.log("app.js connected");
 function buttonClick() {
   d3.text('/magic').then (function(d){
     // console.log(d)
-    document.getElementById("prediction").textContent = ('Our Random Forest Model Predicts the Income to be: '  + d)
+    document.getElementById("prediction").textContent = (d)
   });
 }
 
 function clearButton() {
   var pTag = (document.getElementById('prediction').innerHTML = '');
 }
-
-
